029d3adac0c8476342c137b5925cc15e20389527
FreeBz <freebz@hananet.net>
2021-11-12 14:17:25 +0900

    6장 불(boolean) 타입

---
+++ ch06/ex6-1.js
@@ -0,0 +1,12 @@
+// 불(boolean) 타입
+
+undefined < null		// false
+undefined > null		// false
+undefined === null		// false
+
+NaN === NaN			// false
+NaN !== NaN			// true
+
+"11" < "2"			// true
+"2" < 5				// true
+5 < "11"			// true
